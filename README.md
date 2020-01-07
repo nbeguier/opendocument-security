99f4fd33e1d1019ac1d6e7f9d597d65319ecd3e4
nbeguier <nicolas_beguier@hotmail.com>
2020-01-07 22:12:59 +0100

    Add exceptions and better display of macro

--- README.md
+++ README.md
@@ -8,15 +8,21 @@ pip3 install -r requirements.txt
 
 ### Usage
 
-$ ./opendocument-security.py examples/malware2.odt 
+$ ./opendocument-security.py examples/vba-nested-ole.odt 
+> Parsing ./examples/vba-nested-ole.odt
+> This OpenDocument contains macro !
+  > Object 2/Basic/Standard/TEST.xml
+  > Object 2/Basic/Standard/script-lb.xml
+  > Basic/Standard/TEST.xml
+  > Basic/Standard/script-lb.xml
 > Entering in content.xml
-[CRITICAL] Found event listener: {'{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'dom:load', '{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple'}
-[CRITICAL] Found event listener: {'{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'office:save', '{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple'}
+  > Found event listener: {'{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'dom:load'}
+  > Found event listener: {'{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'office:save'}
   > Entering in Object 1/content.xml
   > Exiting Object 1/content.xml
   > Entering in Object 2/content.xml
-[CRITICAL] Found event listener: {'{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'dom:load', '{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple'}
-[CRITICAL] Found event listener: {'{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'office:save', '{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple'}
+    > Found event listener: {'{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'dom:load'}
+    > Found event listener: {'{http://www.w3.org/1999/xlink}href': 'vnd.sun.star.script:Standard.TEST.Flag?language=Basic&location=document', '{http://www.w3.org/1999/xlink}type': 'simple', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}language': 'ooo:script', '{urn:oasis:names:tc:opendocument:xmlns:script:1.0}event-name': 'office:save'}
   > Exiting Object 2/content.xml
 > Exiting content.xml
 ```
--- opendocument-security.py
+++ opendocument-security.py
@@ -13,37 +13,76 @@ import xml.etree.ElementTree as ET
 # Debug
 # from pdb import set_trace as st
 
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 
+logging.basicConfig(format='%(message)s')
 LOGGER = logging.getLogger('opendocument-security')
 
+def display_macro(od_zipfile):
+    """
+    This function returns the list of existing macro
+    """
+    display_banner = False
+    for filepath in od_zipfile.namelist():
+        if 'Basic/' in filepath:
+            if not display_banner:
+                LOGGER.critical('> This OpenDocument contains macro !')
+                display_banner = True
+            LOGGER.critical('  > %s', filepath)
+        if 'Scripts/' in filepath:
+            if not display_banner:
+                LOGGER.critical('> This OpenDocument contains macro !')
+                display_banner = True
+            LOGGER.critical('  > %s', filepath)
+    return display_banner
+
 def find_rec(node, element, result):
+    """
+    This function is parsing the xml node to update the
+    result by a list of found elements
+    """
     for item in node:
         for match_item in item.findall(element):
             result.append(match_item)
         find_rec(item, element, result)
     return result
 
-def get_event_listeners(content, script_prefix):
+def get_event_listeners(content, script_prefix, indent):
+    """
+    This function returns every script:event-listeners in the document
+    """
     result = list()
     find_rec(content, '{'+script_prefix+'}'+'event-listener', result)
     for script in result:
-        LOGGER.critical('[CRITICAL] Found event listener: %s', script.attrib)
+        LOGGER.critical('%s> Found event listener: %s', indent, script.attrib)
 
 def get_ole_objects(content, draw_prefix):
+    """
+    This function returns the nested ole objects in the document
+    """
     result = list()
     return find_rec(content, '{'+draw_prefix+'}'+'object', result)
 
 def get_tag(content, tag):
-    for i in content.decode().split(' '):
+    """
+    This function returns the tag value
+    """
+    for i in content.decode('utf-8', 'ignore').split(' '):
         if i.startswith(tag+'='):
             return i.split('"')[-2]
 
-def main(odt_zipfile, content_path, indent=''):
+def display_event_listener(od_zipfile, content_path, indent=''):
+    """
+    This function is parsing the file and displaying macro in event-listener
+    """
     LOGGER.warning('%s> Entering in %s', indent, content_path)
-    with odt_zipfile.open(content_path) as manifest:
-        raw_content = manifest.read()
-        root = ET.fromstring(raw_content)
+    with od_zipfile.open(content_path) as content:
+        raw_content = content.read()
+        try:
+            root = ET.fromstring(raw_content)
+        except ET.ParseError:
+            LOGGER.critical('This file seems corrupted')
+            return False
         office_prefix = get_tag(raw_content, 'xmlns:office')
         script_prefix = get_tag(raw_content, 'xmlns:script')
         draw_prefix = get_tag(raw_content, 'xmlns:draw')
@@ -52,14 +91,29 @@ def main(odt_zipfile, content_path, indent=''):
             LOGGER.warning('%s> Exiting %s', indent, content_path)
             return False
         if script_prefix is not None:
-            get_event_listeners(root, script_prefix)
+            get_event_listeners(root, script_prefix, indent+'  ')
         if draw_prefix and xlink_prefix is not None:
             for ole_object in get_ole_objects(root, draw_prefix):
                 draw_href = ole_object.attrib['{'+xlink_prefix+'}'+'href'].replace('./', '')
-                main(odt_zipfile, '{}/{}'.format(draw_href, 'content.xml'), indent=indent+'  ')
+                display_event_listener(
+                    od_zipfile,
+                    '{}/{}'.format(draw_href, 'content.xml'),
+                    indent=indent+'  ')
     LOGGER.warning('%s> Exiting %s', indent, content_path)
+    return True
 
 if __name__ == '__main__':
-    ODT_PATH = sys.argv[1]
-    with zipfile.ZipFile(ODT_PATH, 'r') as ODT_ZIP_FILE:
-        main(ODT_ZIP_FILE, 'content.xml')
+    OD_PATH = sys.argv[1]
+    try:
+        OD_ZIP_FILE = zipfile.ZipFile(OD_PATH, 'r')
+    except IOError:
+        LOGGER.critical('This file seems corrupted')
+        sys.exit(1)
+    except (zipfile.BadZipfile, zipfile.LargeZipFile) as err:
+        LOGGER.critical(err)
+        sys.exit(1)
+    LOGGER.warning('> Parsing %s', OD_PATH)
+    IS_MACRO = display_macro(OD_ZIP_FILE)
+    if IS_MACRO:
+        display_event_listener(OD_ZIP_FILE, 'content.xml')
+    OD_ZIP_FILE.close()
